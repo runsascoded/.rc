fc66e59d84b6ebf44edecf9c328c73157114954e
Ryan Williams <ryan.blake.williams@gmail.com>
2014-05-21 00:23:52 -0400

    factor get git remotes helper out to own file

---
+++ s/git/util/remotes.py
@@ -0,0 +1,15 @@
+
+import re
+import subprocess
+
+push_remote_re = '(?P<name>[^\s]+)\s+((?P<user>[^@]+)@)?(?P<host>[^:]+):(?P<path>[^\s]+)\s\\(push\\)'
+
+def get_remotes():
+  remote_lines = subprocess.Popen(['git', 'remote', '-v'], stdout=subprocess.PIPE).communicate()[0].split('\n')
+  remotes = {}
+  map(
+    lambda remote: remotes.update({remote.group('name'): remote}), 
+    filter(lambda x: x, map(lambda line: re.match(push_remote_re, line), remote_lines))
+  )
+  return remotes
+
