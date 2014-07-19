c44a9a95b1f73a8ef41a87ad2a57add06e0f8643
Ryan Williams <ryan.blake.williams@gmail.com>
2014-07-19 03:26:48 -0400

    `col` supports more pythonic slice-range syntax

--- s/col.py
+++ s/col.py
@@ -4,16 +4,31 @@ import fileinput
 import re
 import sys
 
-cols = map(int, sys.argv[1].split(','))
+def parse_col(arg):
+    segments = arg.split(':')
+    if len(segments) == 1:
+        start = 2*int(arg)
+        end = 2*int(arg) + 2
+        return (start, None if not end else end)
+    if len(segments) == 2:
+        return (
+            None if segments[0] == '' else (2*int(segments[0])),
+            None if segments[1] == '' else (2*int(segments[1]))
+        )
+    raise Exception('Malformed arg: %s' % arg)
+
+cols = map(parse_col, sys.argv[1].split(','))
 sys.argv = sys.argv[1:]
 
 for line in fileinput.input():
-    l = re.split('\s+', line.strip())
+    l = re.split('(\s+)', line.strip())
+    offset = 0
+    if l and not l[0]:
+        offset = 1
+    # print "line: %s, cols: %s" % (l, cols)
     for col in cols:
-        if abs(col) >= len(l):
-            continue
-
-        # print l
-        # print col
-        print l[col],
+        slice = l[
+                (None if col[0] == None else col[0] + offset):(None if col[1] == None else col[1] + offset)
+        ]
+        sys.stdout.write(''.join(slice).strip())
     print ''
