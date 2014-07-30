86de8ce66efa99b7788e828e51f6ae021d184117
Ryan Williams <ryan.blake.williams@gmail.com>
2014-07-30 13:57:34 -0400

    add delimiter options to `col`

--- s/col.py
+++ s/col.py
@@ -1,8 +1,48 @@
 #!/usr/bin/python
 
+import argparse
 import fileinput
 import re
 import sys
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+parser.add_argument(
+    '-i',
+    '-in',
+    '--in-seperator',
+    #metavar='in',
+    dest="input_delimiter",
+    #action="store_str",
+    type=str,
+    help="input field seperator regex",
+    default='\s+'
+)
+
+parser.add_argument(
+    '-o',
+    '-out',
+    "--output-delimiter",
+    #metavar="out",
+    dest="output_delimiter",
+    type=str,
+    help="output field delimiter",
+    default=" "
+)
+
+parser.add_argument(
+    '-k',
+    '--keep-original-range-spacing',
+    dest="keep_original_range_spacing",
+    #type=bool,
+    action="store_true",
+    help="When true, will preserve input's (white-)spacing in colon-delimited columnar ranges",
+    default=False
+)
+
+args, unknown = parser.parse_known_args()
+
+output_delimiter = args.output_delimiter.decode('string-escape')
 
 def parse_col(arg):
     segments = arg.split(':')
@@ -17,18 +57,35 @@ def parse_col(arg):
         )
     raise Exception('Malformed arg: %s' % arg)
 
-cols = map(parse_col, sys.argv[1].split(','))
-sys.argv = sys.argv[1:]
+cols = map(parse_col, unknown[0].split(','))
+sys.argv = unknown[0:]
 
 for line in fileinput.input():
-    l = re.split('(\s+)', line.strip())
+    l = re.split('(%s)' % args.input_delimiter, line.strip())
+
     offset = 0
-    if l and not l[0]:
-        offset = 1
+
     # print "line: %s, cols: %s" % (l, cols)
+
+    first = True
     for col in cols:
-        slice = l[
-                (None if col[0] == None else col[0] + offset):(None if col[1] == None else col[1] + offset)
-        ]
-        sys.stdout.write(''.join(slice).strip())
+
+        if args.keep_original_range_spacing:
+            slice = l[
+                    (None if col[0] == None else col[0] + offset):(None if col[1] == None else col[1] + offset)
+            ]
+            joiner = ''
+        else:
+            slice = l[
+                    (None if col[0] == None else col[0] + 1):(None if col[1] == None else col[1] + 1):2
+            ]
+            joiner = output_delimiter
+
+        # print slice
+
+        if not first:
+            sys.stdout.write(output_delimiter)
+        first = False
+
+        sys.stdout.write(joiner.join(slice).strip())
     print ''
