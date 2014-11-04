b35304acfbeb6f66cb34d2a97c477bfa6d725325
Ryan Williams <ryan.blake.williams@gmail.com>
2014-11-04 18:42:17 -0500

    JS helpers

---
+++ s/datetime-helpers/sort-dates.js
@@ -0,0 +1,50 @@
+
+var moment = require('moment');
+
+var args = process.argv.slice(2);
+
+var dropNoDateLines = true;
+
+process.stdout.on('error', function( err ) {
+    if (err.code == "EPIPE") {
+        process.exit(0);
+    }
+});
+
+function parseDateFormat(str) {
+  var m = str.match(/([0-9]{2,4})([/-])[0-9]{2}[/-][0-9]{2} [0-9]{2}:[0-9]{2}(:[0-9]{2})?/)
+  if (m) {
+    var yearFormat = m[1].split('').map(function() { return 'Y'; }).join('');
+    var dateSep = m[2];
+    var secondsFormatArray = m[3] ? ['ss'] : []
+    var format = [yearFormat, 'MM', 'DD'].join(dateSep) + " " + ['HH', 'mm'].concat(secondsFormatArray).join(':')
+    return format;
+  }
+}
+
+var content = '';
+process.stdin.resume();
+process.stdin.on('data', function(buf) { content += buf.toString(); });
+process.stdin.on('end', function() {
+
+  var lines = content.split("\n").filter(function(line) { return !!line; });   // split the lines
+  lines.map(function(line) {
+    var format = parseDateFormat(line);
+    if (!format) {
+      if (dropNoDateLines) {
+        return undefined;
+      }
+      throw new Error("Couldn't parse datetime: " + line);
+    }
+    var m = moment(line, format);
+    var unix = parseInt(m.format('X'));
+    return [unix, line];
+  }).filter(function(x) {
+    return !!x;
+  }).sort(function(p1, p2) {
+    return p1[0] - p2[0];
+  }).forEach(function(p) {
+    console.log(p[1]);
+  });
+});
+
