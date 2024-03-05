ef2e8dc49d26e7ad41850200386a0a79fef5b160
Ryan Williams <ryan@runsascoded.com>
2024-03-05 10:05:48 -0500

    rc-merge-server.sh push fix

--- rc-merge-server.sh
+++ rc-merge-server.sh
@@ -4,15 +4,14 @@ set -ex
 
 git checkout gh-server
 git merge --no-edit gh-all
-
 git push gh
 
 git checkout gl-all
 git merge --no-edit gh-all
+git push gl
 
 git checkout gl-server
 git merge --no-edit gl-all
-
 git push gl
 
 git checkout gh-all
