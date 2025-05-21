df0ca54a3287f4d95052857aeb7fc978b5818159
Ryan Williams <ryan@runsascoded.com>
2025-05-20 20:02:10 -0400

    arg+1, av+3, aws+9, docker+1, git+7, jar+1, js+2, kubectl+1, py+4
    - arg: 4fb7ea0..f3627ff
      - f3627ff: `trdr="tr -d r"`
    - av: b43df67..6d17e18
      - 6d17e18: `vsb="video-stats.py -H"`
      - e4dcf71: `speed-up` fixes, default framerate 30
      - 5e2af0d: `ocr` wrapper for `tesseract`
    - aws: 6dfb04f..75c5db0
      - 75c5db0: `aws-creds.py`: support `env` output format
      - 3f354ae: `aws-creds.py`: optional cache skip, fix default-profile fallback
      - d2ea3d4: `aws-creds.py`
      - 4fb80b7: rm `aws-vault` aliases
      - 324e1e9: `awr=aws_region` can get or set
      - 1aa9a06: `alu="aws iam list-users"`, `alk="aws iam list-access-keys --user-name"`
      - 67b52ea: add readme
      - 3ee6438: `aws-vault` aliases
      - 7f61f9e: `agmc=aws_get_mfa_creds`
    - docker: 2154391..c214c07
      - c214c07: `docker-build.py`: improve dbg-cmd logging
    - git: f54f08f..119decb
      - 119decb: rm duplicate `gsacn` alias
      - 2853dc2: rm `diff-pipe`
      - 0ab7cbf: `diff.noprefix` aliases
      - 4ae99e6: `update-readme.sh -D`
      - 3895a40: `lpf = log -p --follow`, `lpw = log -p -w`
      - 6697c55: `cobt`: fix usage str
      - e6d5a75: `config-diff-command`: log cmd to stderr before executing
    - jar: a844f49..66a9f23
      - 66a9f23: `java_home`: handle errors
    - js: 83fb5e2..7d200b7
      - 7d200b7: `ycjr="yq -pc -oj -r"`
      - 330d120: `human_readable_sum` aliases
    - kubectl: b038709..cdc5206
      - cdc5206: `get` aliases
    - py: f13fa8c..4f9cf29
      - 4f9cf29: `jki="python -m ipykernel install"`
      - da7943b: cmd dbg nit
      - 7b8dfaa: `PYTHON_DEFAULT_VERSION=3.12.9`
      - 07b7ea0: `dsr="datasette --reload --open"`

--- .githooks/post-checkout.sh
+++ .githooks/post-checkout.sh
@@ -20,7 +20,7 @@ if [ -z "$new_branch" ]; then
   exit 0
 fi
 
-submodules=("dvc" "parquet" "py")
+submodules=(aws dvc parquet py)
 
 # Function to handle submodule checkouts
 submodules_checkout() {
